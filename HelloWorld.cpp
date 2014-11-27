ee4da062bcfadc0dbf68469a6f04bf58c9a6650d
Isak14 <isakgrims@gmail.com>
2014-11-27 13:25:40 +0000

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,3 +6,11 @@ int main()
 {
   cout << "Hello World!" << endl;
 }
+
+UI()
+{
+	 [Input V display V search]
+	 input --> input?
+	 display list --> order? --> display list(ordered) --> order?
+	 search? --> display list(searched) --> order? --> display list(searched, ordered) -- search?
+}
